6dfe905dc1e2d5729ecddb2ea223c98bcd8e5aaf
Hunter Valentine <107652155+HunterValentine98@users.noreply.github.com>
2022-08-04 15:39:27 -0400

    Add files via upload

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,45 +1,33 @@
--- Deliverable 1
-SELECT ce.emp_no,
+--Deliverable 1
+SELECT e.emp_no,
 
-  ce.first_name,
+  e.first_name,
 
-  ce.last_name,
+  e.last_name,
 
-  ti.title,
+  t.title,
 
-  ti.from_date,
+  t.from_date,
 
-  ti.to_date
-
+  t.to_date
 INTO retirement_titles
-
-FROM employees AS ce
-
-  INNER JOIN titles AS ti
-
-    ON (ce.emp_no = ti.emp_no)
-
+FROM employees AS e
+INNER JOIN titles AS t
+    ON (e.emp_no = t.emp_no)
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
-
-ORDER BY ce.emp_no;
+ORDER BY e.emp_no;
 
 
 SELECT * FROM retirement_titles
 
-
-
-
-
-
-SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
-
+SELECT DISTINCT ON(rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
 INTO unique_titles
-
-FROM retirement_titles
-
+FROM retirement_titles as rt
 WHERE to_date = ('9999-01-01')
-
-ORDER BY emp_no ASC;
+ORDER BY rt.emp_no, rt.to_date DESC;
 
 
 
@@ -51,14 +39,10 @@ SELECT * FROM unique_titles
 
 
 
-SELECT COUNT(title), title
-
+SELECT COUNT(ut.title), ut.title
 INTO retiring_titles
-
-FROM unique_titles
-
-GROUP BY title
-
+FROM unique_titles AS ut
+GROUP BY ut.title
 ORDER BY count DESC;
 
 
@@ -106,3 +90,4 @@ ORDER BY e.emp_no;
 
 
 SELECT * FROM mentorship_eligibility
+
